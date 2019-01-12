1918952d90919f846a8b035a0dd586df6d86fcc3
robbert <robbert.schuitema@gmail.com>
2019-01-12 08:19:53 +0100

    increase lines of code for multiply function

--- source/c/component_c/componentc.c
+++ source/c/component_c/componentc.c
@@ -29,5 +29,35 @@ int multiply(int p1, int p2, int p3, int p4)
 	sum += p3;
 	sum += p4;
 
+	sum += p1;
+	sum += p2;
+	sum += p3;
+	sum += p4;
+	
+	sum += p1;
+	sum += p2;
+	sum += p3;
+	sum += p4;
+
+ 	sum += p1;
+	sum += p2;
+	sum += p3;
+	sum += p4;
+
+	sum += p1;
+	sum += p2;
+	sum += p3;
+	sum += p4;
+	
+	sum += p1;
+	sum += p2;
+	sum += p3;
+	sum += p4;
+
+ 	sum += p1;
+	sum += p2;
+	sum += p3;
+	sum += p4;
+
 	return 0;
 }

7f9706b1fa6d5b5b4506ef7476046e8772159930
robbert <robbert.schuitema@gmail.com>
2019-01-12 09:10:05 +0100

    increase lines of code

--- source/c/component_b/componentb.c
+++ source/c/component_b/componentb.c
@@ -156,3 +156,107 @@ int subtract3(int p1, int p2)
 
 	return 0;
 }
+
+int subtract4(int p1, int p2)
+{
+	int sum =0;
+	
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+	
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+ 	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+	
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+ 	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+	
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+ 	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+	return 0;
+}
+
+int subtract5(int p1, int p2)
+{
+	int sum =0;
+	
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+	
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+ 	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+	
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+ 	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+	
+	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+ 	sum += p1;
+	sum += p2;
+	sum += p1;
+	sum += p2;
+
+	return 0;
+}
